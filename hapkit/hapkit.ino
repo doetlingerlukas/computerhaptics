d200f76ac7dfc5d78877d350036e891b8a49aaa1
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2020-09-14 13:19:14 +0200

    Refactor position calculation.

--- hapkit/hapkit.ino
+++ hapkit/hapkit.ino
@@ -22,18 +22,11 @@ int minVal = 48;
 int maxVal = 972;
 
 // Position tracking variables
-int updatedPos = 0;     // keeps track of the latest updated value of the MR sensor reading
-int rawPos = 0;         // current raw reading from MR sensor
-int lastRawPos = 0;     // last raw reading from MR sensor
-int lastLastRawPos = 0; // last last raw reading from MR sensor
-int flipNumber = 0;     // keeps track of the number of flips over the 180deg mark
-int tempOffset = 0;
-int rawDiff = 0;
-int lastRawDiff = 0;
-int rawOffset = 0;
-int lastRawOffset = 0;
-const int flipThresh = 700;  // threshold to determine whether or not a flip over the 180 degree mark occurred
-boolean flipped = false;
+int absPos = 0;
+int rawPos = 0;
+int lastRawPos = 0;
+int flipNumber = 0;
+const int flipThresh = 300;
 
 void setup() {
   // Set up serial communication
@@ -54,34 +47,19 @@ void setup() {
   digitalWrite(dirPin, LOW);  // set direction
 
   // Initialize position valiables
-  lastLastRawPos = analogRead(sensorPin);
   lastRawPos = analogRead(sensorPin);
 }
 
 void loop() {
   updatePos();
-  calcAbsPos();
-
-  /*Serial.print("Max: ");
-  Serial.print(maxVal);
-  Serial.print("   Min: ");
-  Serial.print(minVal);
-  Serial.print("\n");*/
-  //delay(50);
-}
-
-void calcAbsPos() {
-  int absPos = rawPos + (flipNumber * maxVal);
-
-  Serial.print("Pos: ");
-  Serial.print(absPos);
-  Serial.print("\n");
+  calcAngle();
 }
 
 void updatePos() {
   // Get voltage output by MR sensor
   rawPos = analogRead(sensorPin);  //current raw position from MR sensor
 
+  // Update min and max values of MR sensor
   if (rawPos > maxVal) {
     maxVal = rawPos;
   }
@@ -89,59 +67,56 @@ void updatePos() {
     minVal = rawPos;
   }
 
-  // Calculate differences between subsequent MR sensor readings
-  rawDiff = rawPos - lastRawPos;          //difference btwn current raw position and last raw position
-  lastRawDiff = rawPos - lastLastRawPos;  //difference btwn current raw position and last last raw position
-  rawOffset = abs(rawDiff);
-  lastRawOffset = abs(lastRawDiff);
-  
-  // Update position record-keeping vairables
-  lastLastRawPos = lastRawPos;
-  lastRawPos = rawPos;
-  
-  // Keep track of flips over 180 degrees
-  if((lastRawOffset > flipThresh) && (!flipped)) { // enter this anytime the last offset is greater than the flip threshold AND it has not just flipped
-    if(lastRawDiff > 0) {        // check to see which direction the drive wheel was turning
-      flipNumber--;              // cw rotation 
-    } else {                     // if(rawDiff < 0)
-      flipNumber++;              // ccw rotation
+  // Calculate position difference
+  int rawDiff = rawPos - lastRawPos;
+  int rawOffset = abs(rawDiff);
+
+  // Update absolute position
+  if((rawOffset > flipThresh)) {
+    if(rawDiff > 0) {
+      flipNumber--;
+      absPos = absPos + (rawPos - maxVal) + (minVal - lastRawPos);
+    } else {
+      flipNumber++;
+      absPos = absPos + (maxVal - lastRawPos) + (rawPos - minVal);
     }
-    if(rawOffset > flipThresh) { // check to see if the data was good and the most current offset is above the threshold
-      updatedPos = rawPos + flipNumber*rawOffset; // update the pos value to account for flips over 180deg using the most current offset 
-      tempOffset = rawOffset;
-    } else {                     // in this case there was a blip in the data and we want to use lastactualOffset instead
-      updatedPos = rawPos + flipNumber*lastRawOffset;  // update the pos value to account for any flips over 180deg using the LAST offset
-      tempOffset = lastRawOffset;
-    }
-    flipped = true;            // set boolean so that the next time through the loop won't trigger a flip
-  } else {                        // anytime no flip has occurred
-    updatedPos = rawPos + flipNumber*tempOffset; // need to update pos based on what most recent offset is 
-    flipped = false;
+  } else {
+    absPos += rawDiff;
   }
 
+  Serial.print("Pos: ");
+  Serial.print(absPos);
+  Serial.print("\n");
+
+  // Set last raw position
+  lastRawPos = rawPos;
+}
+
+void calcAngle() {
+  double alpha = absPos / 77.13;
+
+  Serial.print("Angle: ");
+  Serial.print(alpha);
+  Serial.print("\n");
 }
 
 /*
     calPosMeter()
 */
-void calPosMeter()
-{
+void calPosMeter() {
  
 }
 /*
     forceRendering()
 */
-void forceRendering()
-{
+void forceRendering() {
   // Add the function for force calculation here.
 }
 
 /*
       Output to motor
 */
-void motorControl()
-{
-
+void motorControl() {
   Tp = rp / rs * rh * force;  // Compute the require motor pulley torque (Tp) to generate that force
   // Determine correct direction for motor torque
   // You may need to reverse the digitalWrite functions according to your motor connections
